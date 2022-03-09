b11ae2205ce6cb21e35fd00fcd9a508e0e695601
AFKL-CUIT <afkl1919@gmail.com>
2022-03-09 14:57:07 +0800

    Support for submitting multiple targets at once.

--- main.go
+++ main.go
@@ -16,36 +16,40 @@ func main() {
 	app := cli.App("FumoFinger", "OMG! The FUMO! ᗜˬᗜ")
 	app.Version("v version", VERSION)
 	var (
-		target     = app.StringOpt("t target", "", "Set the target url.")
-		targetFile = app.StringOpt("r file", "", "Set the target list from file.")
-		thread     = app.IntOpt("threads", 10, "Threads number.")
+		targets      = app.StringsOpt("t target", []string{}, "Set the target url.")
+		targetsFiles = app.StringsOpt("r file", []string{}, "Set the target list from file.")
+		thread       = app.IntOpt("threads", 10, "Threads number.")
 		// timeout    = app.IntOpt("timeout", 20, "Request timeout.")
 		fumo = app.StringOpt("fumo", "fumo", "fumo")
 	)
 	// app.Spec = "-v | (-t=<target> | -r=<target file>) [--threads=<threads>] [--timeout=<timeout>] | --fumo=<fumo>"
-	app.Spec = "-v | --fumo=<fumo> | (-t=<target> | -r=<target file>) [--threads=<threads>]"
+	app.Spec = "-v | --fumo=<fumo> | (-t=<target> | -r=<target file>)... [--threads=<threads>]"
 	app.Action = func() {
 		if *fumo == "fumo" {
 			targetNum := 0
-			targets := new(model.TargetList)
-			if len(*target) != 0 {
-				targets.LoadTargetFromString(*target)
-				targetNum += 1
+			execTargets := new(model.TargetList)
+			if len(*targets) != 0 {
+				for _, target := range *targets {
+					execTargets.LoadTargetFromString(target)
+					targetNum += 1
+				}
 			}
 
-			if len(*targetFile) != 0 {
-				num, err := targets.LoadTargetFromFile(*targetFile)
-				if err != nil {
-					log.Fatalln(err.Error())
+			if len(*targetsFiles) != 0 {
+				for _, file := range *targetsFiles {
+					num, err := execTargets.LoadTargetFromFile(file)
+					if err != nil {
+						log.Fatalln(err.Error())
+					}
+					targetNum += num
 				}
-				targetNum += num
 			}
 
 			if targetNum == 0 {
 				log.Fatalln("target number is 0")
 			}
 
-			core.Start(*targets, *thread)
+			core.Start(*execTargets, *thread)
 			core.Wait()
 			core.End()
 		} else {
